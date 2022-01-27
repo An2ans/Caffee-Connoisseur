212ebfaab27302fd54759979468e546a3122ae45
An2ans <antoniojesus_92@hotmail.com>
2022-01-27 19:27:20 +0000

    added geolocation functions but foursquares API url is not working

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,42 @@ import styles from '../styles/Home.module.css';
 import Card from "../components/card.jsx";
 import Banner from "../components/banner.jsx";
 import { fetchCoffeeStores } from '../lib/coffee-stores';
+import useTrackLocation from '../lib/use-track-location';
+import { useEffect } from 'react';
 
 
 export async function getStaticProps(context){
   const coffeeStores = await fetchCoffeeStores();
-  console.log(coffeeStores);    
-  
   return{
     props:{
       coffeeStores: coffeeStores,
-    },
+    }
   };
 }
 
 
 
 export default function Home(props) {
+
+  const {handleTrackLocation, latLong, errorMsg, isFinding} = useTrackLocation();
+
+  useEffect(async () => {
+    if(latLong){
+      try{
+        const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
+        console.log({fetchCoffeeStores});
+      }catch(error){
+        console.log({error})
+      }
+    }
+  }, [latLong]);
+
+
   const handleBannerClick = (e) => {
-    console.log(e);
+    handleTrackLocation();
   };
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -32,21 +49,24 @@ export default function Home(props) {
       </Head>
 
       <main className={styles.main}>
-        <Banner handleClick={handleBannerClick} buttonText="View stores nearby" />
+        <Banner handleClick={handleBannerClick} buttonText={isFinding ? "Locating..." : "View stores nearby"} />
         <div className={styles.heroImage}>
           <Image src="/coffee.png" alt="coffee" width={300} height={200} />
         </div>
-        {props.coffeeStores.length > 0 && (
-          <h2 className={styles.heading2} > Toronto stores </h2>
-        )}
-        <div className={styles.cardLayout}>
-          {props.coffeeStores.map(store => <Card 
-            name={store.name}
-            imgUrl={store.imgUrl} 
-            href={`/store/${store.id}`}
-            className={styles.card}
-            key={store.id} />
+        {errorMsg && <p>Something went wrong: {errorMsg} </p>}
+        <div className={styles.sectionWrapper}>
+          {props.coffeeStores.length > 0 && (
+            <h2 className={styles.heading2} > Toronto stores </h2>
           )}
+          <div className={styles.cardLayout}>
+            {props.coffeeStores.map(store => <Card 
+              name={store.name}
+              imgUrl={store.imgUrl || "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"} 
+              href={`/store/${store.id}`}
+              className={styles.card}
+              key={store.id} />
+            )}
+          </div>
         </div>
       </main>
       <footer className={styles.footer}>
--- pages/store/[id].js
+++ pages/store/[id].js
@@ -1,23 +1,25 @@
 import {useRouter} from "next/router";
 import Link from "next/link";
-import coffeeStore from "../../data/coffee-stores.json";
 import Head from "next/head";
 import styles from "../../styles/store.module.css";
 import Image from "next/image";
 import cls from "classnames";
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
-export function getStaticProps({params}){
+export async function getStaticProps({params}){
+  const coffeeStores = await fetchCoffeeStores();
   return{
     props: {
-      coffeeStore: coffeeStore.find( store => {
-        return store.id == params.id;
+      coffeeStores: coffeeStores.find( store => {
+        return store.id.toString() === params.id;
       }),
     },
   };
 }
 
-export function getStaticPaths(){
-  const paths = coffeeStore.map(store => {
+export async function getStaticPaths(){
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = await coffeeStores.map(store => {
     return{
       params: {
         id: store.id.toString(),
@@ -37,12 +39,10 @@ const Store = (props) => {
       return <div>Loading...</div>
     }
 
-    const {address, name, neighbourhood, imgUrl} = props.coffeeStore;
+    const {address, neighborhood, name, imgUrl} = props.coffeeStores;
 
     const handleUpvote = () => {
-
     }
-
 
     return (
       <div className={styles.layout}>
@@ -52,22 +52,22 @@ const Store = (props) => {
         <div className={styles.container} >
           <div className={styles.col1}>
             <Link href="/" className={styles.backToHomeLink} >
-                <a>Back to home</a>
+                <a>&#8592; Back to home</a>
             </Link>
             <div className={styles.nameWrapper}>
               <h1 className={styles.name}>{name}</h1>
             </div>
-            <Image src={imgUrl} width={600} height={360} className={styles.storeImg} alt={name} />
+            <Image src={imgUrl || "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"} width={600} height={360} className={styles.storeImg} alt={name} />
           </div>
           <div className={cls(styles.col2, "glass")}>
             <div className={styles.iconWrapper}>
-              <Image src={"/icons/nearMe.svg"} width={24} height={24} alt={address} />
+              <Image src={"/icons/places.svg"} width={24} height={24} alt={address} />
               <p className={styles.text}>{address}</p>
             </div>
-            <div className={styles.iconWrapper}>
-              <Image src={"/icons/places.svg"} width={24} height={24} alt={neighbourhood} />
-              <p className={styles.text}>{neighbourhood}</p>
-            </div>
+            {neighborhood && (<div className={styles.iconWrapper}>
+              <Image src={"/icons/nearMe.svg"} width={24} height={24} alt={neighborhood} />
+              <p className={styles.text}>{neighborhood}</p>
+            </div>)}
             <div className={styles.iconWrapper}>
               <Image src={"/icons/star.svg"} width={24} height={24} alt="score" />
               <p className={styles.text}>{1}</p>
