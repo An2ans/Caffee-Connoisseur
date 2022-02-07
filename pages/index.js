34e2eb1eb9e094111e9b3fe4d2185e13d3f216b0
An2ans <antoniojesus_92@hotmail.com>
2022-02-07 20:11:55 +0000

    added serverless api for fetching coffee stores

--- pages/index.js
+++ pages/index.js
@@ -34,15 +34,16 @@ export default function Home(props) {
   useEffect(async () => {
     if(latLong){
       try{
-        const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
-        console.log({fetchedCoffeeStores});
-        // setCoffeeStores(fetchedCoffeeStores);
+        const response = await fetch(`api/getCoffeeStoresByLocation?latLong=${latLong}&limit=30`)
+        const coffeeStores = await response.json();
+        console.log({response});
         dispatch({
           type:ACTION_TYPES.SET_COFFEE_STORES,
           payload:{
-            coffeeStores: fetchedCoffeeStores
+            coffeeStores,
           }
-        })
+        });
+        setCoffeeStoresError("");
       }catch(error){
         console.log({error})
         setCoffeeStoresError(error.message);
